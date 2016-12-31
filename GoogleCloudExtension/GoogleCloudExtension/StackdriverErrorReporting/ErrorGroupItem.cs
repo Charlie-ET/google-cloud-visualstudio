b2666c1b7f939705d1347626c4096d10de2ce0a5
Deren Liao <derenl@google.com>
2016-12-30 23:31:12 -0800

    Set Users, Status column

--- GoogleCloudExtension/GoogleCloudExtension/StackdriverErrorReporting/ErrorGroupItem.cs
+++ GoogleCloudExtension/GoogleCloudExtension/StackdriverErrorReporting/ErrorGroupItem.cs
@@ -23,22 +23,25 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Linq;
+using System.Text;
 using EventGroupTimeRangeEnum = Google.Apis.Clouderrorreporting.v1beta1.ProjectsResource.GroupStatsResource.ListRequest.TimeRangePeriodEnum;
 
 namespace GoogleCloudExtension.StackdriverErrorReporting
 {
     public class ErrorGroupItem : Model
-    {
+    { 
         public ErrorGroupStats ErrorGroup { get; set; }
-
         public ProtectedCommand NavigateDetailCommand { get; }
         public string Error => ErrorGroup.Representative.Message;
         public long Count => ErrorGroup.Count.GetValueOrDefault();
         public object FirstSeen => ErrorGroup.FirstSeenTime;
         public object LastSeen => ErrorGroup.LastSeenTime;
-        public object SeenIn => "What is this?";
+        public object SeenIn => String.Join(";", ErrorGroup.AffectedServices.Select(x => FormatServiceContext(x)));
         public string Message { get; }
         public string Stack { get; }
+        public long? AffectedUsersCount => ErrorGroup.AffectedUsersCount;
+        public int? Status => ErrorGroup.Representative?.Context?.HttpRequest?.ResponseStatusCode;
 
         // TODO: not necessary?  remove.
         public EventGroupTimeRangeEnum EventGroupTimeRange { get; }
@@ -56,6 +59,24 @@ namespace GoogleCloudExtension.StackdriverErrorReporting
             BarChartModel = new TimedCountBarChartViewModel(errorGroup.TimedCounts, groupTimeRange);
         }
         
+        private string FormatServiceContext(ServiceContext serviceContext)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (!String.IsNullOrWhiteSpace(serviceContext.ETag))
+            {
+                builder.AppendLine(serviceContext.ETag);
+            }
+
+            builder.Append(serviceContext.Service);
+
+            if (!String.IsNullOrWhiteSpace(serviceContext.Version))
+            {
+                builder.Append($".{serviceContext.Version}");
+            }
+
+            return builder.ToString();
+        }
+
         private void NavigateDetail()
         {
             Debug.WriteLine($"{Message} is clicked");
